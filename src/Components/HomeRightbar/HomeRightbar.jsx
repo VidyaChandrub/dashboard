2ee448e31a076b0747fe1bbea79df0b0f5d0d673
Vidya <vidyac266@gmail.com>
2023-10-15 22:26:42 +0530

    search bar & components

--- src/Components/HomeRightbar/HomeRightbar.jsx
+++ src/Components/HomeRightbar/HomeRightbar.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 import "./HomeRightbar.css"
+import Navbar from "../Navbar.jsx";
 
 function HomeRightbar(){
     return(
-        <div className="mainHomerightbar">HomeRightBar</div>
+        <div className="mainHomerightbar">
+            <Navbar/>
+            <div>
+                <div className="ItemContainer">
+                    <div className="ItemContainer1">
+                        <div>
+                            <p className="taskProgress">Earning</p>
+                            <p className="taskcounter">$198k</p>
+                            <p className="month1">|37.8% of this month</p>
+                        </div>
+                        <div></div>
+                    </div>
+                    <div className="ItemContainer1">2</div>
+                    <div className="ItemContainer1">3</div>
+                    <div className="ItemContainer1">4</div>
+                </div>
+                <div></div>
+            </div>
+        </div>
     )
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import "./Navbar.css";
+import hi from './Icons/hi.png';
+import search from './Icons/search.png'
 
 function Navbar(){
     return(
-        <div>Navbar</div>
+        <div className="mainNavbarContainer">
+            <div className="helloContainer">
+                <h2 className="dashtext">Hello Shahrukk <img src={`${hi}`} className="hiIcon" alt=""/> ,</h2>
+            </div>
+            <div className="searchIconContainer">
+                <img src={`${search}`} className="searchIcon" alt=""/>
+                <input type="search" className="searchInput" placeholder="Search" />
+            </div> 
+        </div>
     )
 }
 
