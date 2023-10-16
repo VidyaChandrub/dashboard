d4a1e8770aa90921f717480188d19f3e53ed63ad
Vidya <vidyac266@gmail.com>
2023-10-16 22:06:51 +0530

    rechatrs

--- src/Components/HomeRightbar/HomeRightbar.jsx
+++ src/Components/HomeRightbar/HomeRightbar.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./HomeRightbar.css"
 import Navbar from "../Navbar.jsx";
+import dollarpng from "../Icons/dollarpng.png";
+import order from "../Icons/order.png";
+import balance from "../Icons/balance.png";
+import sale from "../Icons/sale.png";
 
 function HomeRightbar(){
     return(
@@ -10,15 +14,45 @@ function HomeRightbar(){
                 <div className="ItemContainer">
                     <div className="ItemContainer1">
                         <div>
-                            <p className="taskProgress">Earning</p>
-                            <p className="taskcounter">$198k</p>
-                            <p className="month1">|37.8% of this month</p>
+                            <img  className="Dollar" src={`${dollarpng}`} />
                         </div>
-                        <div></div>
+                        <div>
+                            <p className="taskProgress">Earning</p>
+                            <p className="taskcounter"> $198k</p>
+                            <p className="month1"><span className="Green">  <span>&#8593;</span>37.8%</span> this month</p>
+                        </div>
+                        
                     </div>
-                    <div className="ItemContainer1">2</div>
-                    <div className="ItemContainer1">3</div>
-                    <div className="ItemContainer1">4</div>
+                    <div className="ItemContainer1">
+                        <div>
+                            <img  className="Ordernote" src={`${order}`} />
+                        </div>
+                    <div>
+                            <p className="taskProgress">Orders</p>
+                            <p className="taskcounter">$2.4k</p>
+                            <p className="month1"><span className="Red"><span>&#8595;</span>2%</span> this month</p>
+                        </div>
+                    </div>
+                    <div className="ItemContainer1">
+                        <div>
+                            <img  className="Balance" src={`${balance}`} />
+                        </div>
+                    <div>
+                            <p className="taskProgress">Balance</p>
+                            <p className="taskcounter">$2.4k</p>
+                            <p className="month1"><span className="Red"><span>&#8595;</span>2%</span> this month</p>
+                        </div>
+                    </div>
+                    <div className="ItemContainer1">
+                        <div>
+                            <img  className="sale" src={`${sale}`} />
+                        </div>
+                    <div>
+                            <p className="taskProgress">Total Sales</p>
+                            <p className="taskcounter">$89k</p>
+                            <p className="month1"><span className="Green"><span>&#8593;</span>11%</span> this week</p>
+                        </div>
+                    </div>
                 </div>
                 <div></div>
             </div>
