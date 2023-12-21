72a410460878fc3d591bc7f357eafe2c2a37333b
Vidya <vidyac266@gmail.com>
2023-12-21 21:35:45 +0530

    okie

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
-import Home from './Pages/Home/Home';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Navbar} from "./components/Navbar"
+
 
 function App() {
   return(
     <div className='App'>
-        
+        <h1>Hello World !!</h1>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/shop"></Route>
+            <Route path="/cart"></Route>
+          </Routes>
+        </BrowserRouter>
     </div>
   )
 }
