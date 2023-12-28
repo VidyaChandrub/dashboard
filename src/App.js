68ad52b3e499ea2e90d58700d0edeff8ce60c466
Vidya <vidyac266@gmail.com>
2023-12-28 20:51:50 +0530

    class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from "react";
 import Button from "./Button";
 
-export default function App(){
-  return(
-    <>
-    <h1>Hello Congrats...</h1>
-    <Button />
-    </>
-  )
+// export default function App(){
+//   return(
+//     <>
+//     <h1 className="header">Hello Congrats...</h1>
+//     <Button />
+//     </>
+//   )
+// }
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <>
+      <h1>Hi</h1>
+      </>
+    )
+  }
+    
+  
 }
+
+// import { ReactDOM } from "react";
+
+// const JSX = <>
+// <h1>Hello World</h1>
+// </>
+
+// ReactDOM.render(JSX, document.getElementById('root'));
+
+
+
